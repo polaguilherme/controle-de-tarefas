774e7cac1dfd9ef2b66ee6d41abe2fa4652bf9b7
polaguilherme <guilhermepola663@gmail.com>
2024-05-24 17:23:02 -0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <Header />
       <div className="flex flex-col items-center justify-center mt-10 w-full">
         <div className="w-full max-w-2xl">
-          <Tasks tasks={[]} />
+          <Tasks />
         </div>
       </div>
     </>
